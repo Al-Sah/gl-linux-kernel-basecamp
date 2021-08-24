a40ff2062a166e8b23eacab91be0739c60b3b929
Al_Sah <aplexidr@gmail.com>
2021-08-24 20:06:12 +0300

    Exercise06: Add possibility to connect to the GPIO26

--- exercise06/script.sh
+++ exercise06/script.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+if [ -f "/sys/class/gpio/gpio26" ]; then
+    echo " GPIO26 is active ! "
+    echo 26 > /sys/class/gpio/unexport
+fi
+
+echo 26 > /sys/class/gpio/export
+echo high > /sys/class/gpio/gpio26/direction
+echo 1 > /sys/class/gpio/gpio26/active_low
+echo "GPIO26 is ready !!"
