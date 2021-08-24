a4f9ec26ee8f2b87b527c1216c23414eba52a671
Al_Sah <aplexidr@gmail.com>
2021-08-24 21:26:54 +0300

    Exercise06extra: Add basic functions to control hardware

--- exercise06extra/exercise06extra.sh
+++ exercise06extra/exercise06extra.sh
@@ -1,2 +1,55 @@
 #!/bin/bash
 
+
+setup_button(){
+  if [ -e "/sys/class/gpio/gpio26" ]; then echo 26 > /sys/class/gpio/unexport; fi
+  echo 26 > /sys/class/gpio/export
+  echo high > /sys/class/gpio/gpio26/direction
+  echo 1 > /sys/class/gpio/gpio26/active_low
+  echo "Button is ready !!"
+}
+
+setup_led(){
+  if [ -e "/sys/class/gpio/gpio16" ]; then echo 16 > /sys/class/gpio/unexport; fi
+  if [ -e "/sys/class/gpio/gpio20" ]; then echo 20 > /sys/class/gpio/unexport; fi
+  if [ -e "/sys/class/gpio/gpio21" ]; then echo 21 > /sys/class/gpio/unexport; fi
+
+  echo 16 > /sys/class/gpio/export
+  echo out > /sys/class/gpio/gpio16/direction
+  echo 1 > /sys/class/gpio/gpio16/value
+
+  echo 20 > /sys/class/gpio/export
+  echo out > /sys/class/gpio/gpio20/direction
+  echo 1 > /sys/class/gpio/gpio20/value
+
+  echo 21 > /sys/class/gpio/export
+  echo out > /sys/class/gpio/gpio21/direction
+  echo 1 > /sys/class/gpio/gpio21/value
+
+  echo "Led is ready !!"
+}
+
+shutdown_led(){
+  echo 0 > /sys/class/gpio/gpio16/value
+  echo 16 > /sys/class/gpio/unexport
+
+  echo 0 > /sys/class/gpio/gpio20/value
+  echo 20 > /sys/class/gpio/unexport
+
+  echo 0 > /sys/class/gpio/gpio21/value
+  echo 21 > /sys/class/gpio/unexport
+}
+
+
+setup_button
+while true; do
+	if (( 1 == $(cat /sys/class/gpio/gpio26/value) )); then break; fi
+	sleep 0.05
+done
+
+setup_led
+sleep 1
+
+
+echo 26 > /sys/class/gpio/unexport
+shutdown_led
