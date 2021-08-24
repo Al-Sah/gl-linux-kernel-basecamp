24c65870857ea26470db44e388f22881caf7e924
Al_Sah <aplexidr@gmail.com>
2021-08-24 20:21:02 +0300

    Exercise06: Add endless while to read GPIO26 value

--- exercise06/script.sh
+++ exercise06/script.sh
@@ -9,3 +9,22 @@ echo 26 > /sys/class/gpio/export
 echo high > /sys/class/gpio/gpio26/direction
 echo 1 > /sys/class/gpio/gpio26/active_low
 echo "GPIO26 is ready !!"
+
+
+current_state=0
+previous_state=0
+times=0
+
+while true; do
+	current_state=$(cat /sys/class/gpio/gpio26/value)
+	if (( current_state == 1)); then
+	  if ((previous_state == 0)); then
+	    previous_state=1
+	    ((times++))
+      echo "Clicked times $times"
+	  fi
+	else
+	  previous_state=0
+	fi
+	sleep 0.05
+done
