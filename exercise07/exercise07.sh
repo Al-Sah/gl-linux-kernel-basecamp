ea27ab6818da9280a54bced773326b20e5d67ada
Al_Sah <aplexidr@gmail.com>
2021-08-25 19:01:17 +0300

    Exercise07: Init project

---
+++ exercise07/exercise07.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ -e "/sys/class/gpio/gpio26" ]; then
+    echo "GPIO26 was active ! "
+    echo 26 > /sys/class/gpio/unexport
+fi
+
+echo 26 > /sys/class/gpio/export
+echo high > /sys/class/gpio/gpio26/direction
+echo 1 > /sys/class/gpio/gpio26/active_low
+echo "GPIO26 is ready !!"
+
+current_state=0
+previous_state=0
+duration=0
+
+while true; do
+	current_state=$(cat /sys/class/gpio/gpio26/value)
+	if (( current_state == 1)); then
+	  if ((previous_state == 0)); then
+	    previous_state=1
+    else
+      ((duration++))
+      if (( duration >= 20 )); then
+        break;
+      fi
+    fi
+	else
+	  duration=0
+	  previous_state=0
+	fi
+	sleep 0.05
+done
+
+echo 26 > /sys/class/gpio/unexport
