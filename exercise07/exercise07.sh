d94cf091e5089517677a540b2cf6794e88192072
Al_Sah <aplexidr@gmail.com>
2021-08-25 19:10:49 +0300

    Exercise07: Add temperature and time output (raw)

--- exercise07/exercise07.sh
+++ exercise07/exercise07.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-if [ -e "/sys/class/gpio/gpio26" ]; then
-    echo "GPIO26 was active ! "
-    echo 26 > /sys/class/gpio/unexport
-fi
 
+if [ -e "/sys/class/gpio/gpio26" ]; then echo 26 > /sys/class/gpio/unexport; fi
 echo 26 > /sys/class/gpio/export
 echo high > /sys/class/gpio/gpio26/direction
 echo 1 > /sys/class/gpio/gpio26/active_low
-echo "GPIO26 is ready !!"
+echo "GPIO26 is ready !!"; echo ""
 
 current_state=0
 previous_state=0
@@ -19,6 +16,8 @@ while true; do
 	if (( current_state == 1)); then
 	  if ((previous_state == 0)); then
 	    previous_state=1
+	    echo "Time: $(i2cget -y 1 0x68 0x02) $(i2cget -y 1 0x68 0x01) $(i2cget -y 1 0x68 0x00)"
+	    echo "Temperature: $(i2cget -y 1 0x68 0x11)"; echo ""
     else
       ((duration++))
       if (( duration >= 20 )); then
