f552a60479b7ef49939eb5af7bfc8483a895227e
Al_Sah <aplexidr@gmail.com>
2021-09-13 11:04:41 +0300

    Exercise10: Implement 'to_uppercase' function

--- exercise10/part02/src/string_processor.c
+++ exercise10/part02/src/string_processor.c
@@ -9,15 +9,28 @@ MODULE_DESCRIPTION("Kernel module to flip all words in a str and do it uppercase
 MODULE_VERSION("0.1");
 
 
-static char *str = "Hello world";
-module_param(str, charp, 0000);
-
+static char str[256] = "Hello world";
+module_param_string(str, str, 256, 0);
 
 #define MODULE_TAG "string_processor"
 
+static void to_uppercase(char* src){
+    int i;
+    for (i = 0; i < strlen(src); ++i) {
+        if (src[i] >= 97 && src[i] <= 122) // if the char in is the range a-z
+        {
+            src[i] -= 32; // convert to uppercase
+        }
+    }
+}
 
 static int __init string_processor_init(void)
 {
+    char string_to_modify[64];
+    strcpy(string_to_modify, str);
+    printk(KERN_INFO MODULE_TAG": Input str: %s\n", string_to_modify);
+    to_uppercase(string_to_modify);
+    printk(KERN_INFO MODULE_TAG": Output str: %s\n", string_to_modify);
     return -1;
 }
 
