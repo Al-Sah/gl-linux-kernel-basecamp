7b0e0220f55f4670289e9a858e7e0061b1213dfd
Al_Sah <aplexidr@gmail.com>
2021-10-02 12:49:43 +0300

    Exercise14: Add time measurement in kernel space

--- exercise14/part02/src/mem_test.c
+++ exercise14/part02/src/mem_test.c
@@ -3,6 +3,7 @@
 
 #include <linux/slab.h>
 #include <linux/vmalloc.h>
+#include <linux/timekeeping.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Al_Sah");
@@ -28,9 +29,6 @@ static int __init memory_tester_init(void)
     run_kmalloc();
     run_kcalloc();
     run_kzalloc();
-
-    //KMALLOC_MAX_SIZE
-
     run_vmalloc();
     run_vzalloc();
 
@@ -39,139 +37,182 @@ static int __init memory_tester_init(void)
 
     return -1;
 }
-
-/*static void __exit memory_tester_exit(void) {
-    printk(KERN_NOTICE MODULE_TAG " Exited\n");
-}
-module_exit(memory_tester_exit)*/
 module_init(memory_tester_init)
 
 
 
 void run_kmalloc(void){
     s64 allocation_size = 2;
+    char * allocated_memory = NULL;
+    u64 alloc_ns, free_ns;
     while(1){
-        char * allocated_memory = (char *)kmalloc(allocation_size, GFP_KERNEL);
+        alloc_ns = ktime_get_real_ns();
+        allocated_memory = (char *)kmalloc(allocation_size, GFP_KERNEL);
+        alloc_ns = ktime_get_real_ns() - alloc_ns;
+
         if(allocated_memory == NULL){
-            printk(" [kmalloc] Failed to allocate %lld bytes\n", allocation_size);
+            printk(" [kmalloc]: Failed to allocate %lld bytes; alloc time: %llu\n", allocation_size, alloc_ns);
             break;
         }
-        printk(" [kmalloc] Allocated % 12lld bytes\n", allocation_size);
-
         allocated_memory[allocation_size/2] = 111;
         allocation_size *=2;
 
+        free_ns = ktime_get_real_ns();
         kfree(allocated_memory);
+        free_ns = ktime_get_real_ns() - free_ns;
+
+        printk(" [kmalloc]: Allocated % 12lld bytes; alloc time: % 10lld free time: % 10lld\n", allocation_size, alloc_ns, free_ns);
     }
 }
 
 void run_kcalloc(void){
     s64 allocation_size = 2;
+    char * allocated_memory = NULL;
+    u64 alloc_ns, free_ns;
 
     while(1){
-        char * allocated_memory = (char *)kcalloc( sizeof(char), allocation_size, GFP_KERNEL);
+        alloc_ns = ktime_get_real_ns();
+        allocated_memory = (char *)kcalloc( sizeof(char), allocation_size, GFP_KERNEL);
+        alloc_ns = ktime_get_real_ns() - alloc_ns;
+
         if(allocated_memory == NULL){
-            printk(" [kcalloc] Failed to allocate %lld bytes\n", allocation_size);
+            printk(" [kcalloc]: Failed to allocate %lld bytes; alloc time: %llu\n", allocation_size, alloc_ns);
             break;
         }
-        printk(" [kcalloc] Allocated % 12lld bytes\n", allocation_size);
 
         allocated_memory[allocation_size/2] = 111;
         allocation_size *=2;
 
+        free_ns = ktime_get_real_ns();
         kfree(allocated_memory);
+        free_ns = ktime_get_real_ns() - free_ns;
+
+        printk(" [kcalloc]: Allocated % 12lld bytes; alloc time: % 10lld free time: % 10lld\n", allocation_size, alloc_ns, free_ns);
     }
 }
 
 void run_kzalloc(void){
     s64 allocation_size = 2;
+    char * allocated_memory = NULL;
+    u64 alloc_ns, free_ns;
 
     while(1){
-        char * allocated_memory = (char *)kzalloc( allocation_size, GFP_KERNEL);
+        alloc_ns = ktime_get_real_ns();
+        allocated_memory = (char *)kzalloc( allocation_size, GFP_KERNEL);
+        alloc_ns = ktime_get_real_ns() - alloc_ns;
+
         if(allocated_memory == NULL){
-            printk(" [kzalloc] Failed to allocate %lld bytes\n", allocation_size);
+            printk(" [kzalloc]: Failed to allocate %lld bytes; alloc time: %llu\n", allocation_size, alloc_ns);
             break;
         }
-        printk(" [kzalloc] Allocated % 12lld bytes\n", allocation_size);
 
         allocated_memory[allocation_size/2] = 111;
         allocation_size *=2;
 
+        free_ns = ktime_get_real_ns();
         kfree(allocated_memory);
+        free_ns = ktime_get_real_ns() - free_ns;
+
+        printk(" [kzalloc]: Allocated % 12lld bytes; alloc time: % 10lld free time: % 10lld", allocation_size, alloc_ns, free_ns);
     }
 }
 
 
 void run_vmalloc(void){
     s64 allocation_size = 2;
+    char * allocated_memory = NULL;
+    u64 alloc_ns, free_ns;
 
     while(1){
-        char * allocated_memory = (char *)vmalloc(allocation_size);
+        alloc_ns = ktime_get_real_ns();
+        allocated_memory = (char *)vmalloc(allocation_size);
+        alloc_ns = ktime_get_real_ns() - alloc_ns;
+
 
         if(allocated_memory == NULL){
-            printk(" [vmalloc] Failed to allocate %lld bytes\n", allocation_size);
+            printk(" [vmalloc]: Failed to allocate %lld bytes; alloc time: %llu\n", allocation_size, alloc_ns);
             break;
         }
-        printk(" [vmalloc] Allocated % 12lld bytes\n", allocation_size);
 
         allocated_memory[allocation_size/2] = 111;
         allocation_size *=2;
 
+        free_ns = ktime_get_real_ns();
         vfree(allocated_memory);
+        free_ns = ktime_get_real_ns() - free_ns;
+
+        printk(" [vmalloc]: Allocated % 12lld bytes; alloc time: % 10lld free time: % 10lld\n", allocation_size, alloc_ns, free_ns);
     }
 }
 
 void run_vzalloc(void){
     s64 allocation_size = 2;
+    char * allocated_memory = NULL;
+    u64 alloc_ns, free_ns;
 
     while(1){
-        char * allocated_memory = (char *)vzalloc(allocation_size);
+        alloc_ns = ktime_get_real_ns();
+        allocated_memory = (char *)vzalloc(allocation_size);
+        alloc_ns = ktime_get_real_ns() - alloc_ns;
 
         if(allocated_memory == NULL){
-            printk(" [vzalloc] Failed to allocate %lld bytes\n", allocation_size);
+            printk(" [vzalloc]: Failed to allocate %lld bytes; alloc time: %llu\n", allocation_size, alloc_ns);
             break;
         }
-        printk(" [vzalloc] Allocated % 12lld bytes\n", allocation_size);
 
         allocated_memory[allocation_size/2] = 111;
         allocation_size *=2;
 
+        free_ns = ktime_get_real_ns();
         vfree(allocated_memory);
+        free_ns = ktime_get_real_ns() - free_ns;
+
+        printk(" [vzalloc]: Allocated % 12lld bytes; alloc time: % 10lld free time: % 10lld\n", allocation_size, alloc_ns, free_ns);
     }
 }
 
 void run_get_free_pages(void){
     u32 allocation_size = 1;
     ulong page;
+    u64 alloc_ns, free_ns;
 
     while(1){
+        alloc_ns = ktime_get_real_ns();
         page = __get_free_pages(GFP_KERNEL, allocation_size);
+        alloc_ns = ktime_get_real_ns() - alloc_ns;
 
         if(!page){
-            printk(" [__get_free_pages] Failed to allocate %u pages\n", allocation_size);
+            printk(" [__get_free_pages]: Failed to allocate %u pages\n", allocation_size);
             break;
         }
-        printk(" [__get_free_pages] Allocated %u pages\n", allocation_size);
 
+        free_ns = ktime_get_real_ns();
         free_pages(page, allocation_size);
+        free_ns = ktime_get_real_ns() - free_ns;
         allocation_size +=1;
+        printk(" [__get_free_pages]: Allocated % 3d pages; alloc time: % 10lld free time: % 10lld\n", allocation_size, alloc_ns, free_ns);
     }
 }
 
 void run_alloc_pages(void){
     u32 allocation_size = 1;
     struct page * page;
+    u64 alloc_ns, free_ns;
 
     while(1){
+        alloc_ns = ktime_get_real_ns();
         page = alloc_pages(GFP_KERNEL, allocation_size);
+        alloc_ns = ktime_get_real_ns() - alloc_ns;
 
         if(!page){
-            printk(" [alloc_pages] Failed to allocate %u pages\n", allocation_size);
+            printk(" [alloc_pages]: Failed to allocate %u pages\n", allocation_size);
             break;
         }
-        printk(" [alloc_pages] Allocated %u pages\n", allocation_size);
 
+        free_ns = ktime_get_real_ns();
         __free_pages(page, allocation_size);
+        free_ns = ktime_get_real_ns() - free_ns;
         allocation_size +=1;
+        printk(" [alloc_pages]: Allocated % 3d pages; alloc time: % 10lld free time: % 10lld\n", allocation_size, alloc_ns, free_ns);
     }
 }
