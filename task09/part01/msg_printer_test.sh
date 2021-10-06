aeae49eb978bfe3c0dca20e7114475ad7640baf8
Al_Sah <aplexidr@gmail.com>
2021-10-06 20:12:12 +0300

    Task09: Update testing script

--- task09/part01/msg_printer_test.sh
+++ task09/part01/msg_printer_test.sh
@@ -7,3 +7,11 @@ echo "zzz2" | sudo tee /proc/messages_printer/data
 echo "zzz3" | sudo tee /proc/messages_printer/data
 echo "zzz4" | sudo tee /proc/messages_printer/data
 
+echo "300 1000" | sudo tee /sys/class/messages_printer/settings
+
+echo "zzz1" | sudo tee /proc/messages_printer/data
+echo "zzz2" | sudo tee /proc/messages_printer/data
+echo "zzz3" | sudo tee /proc/messages_printer/data
+echo "zzz4" | sudo tee /proc/messages_printer/data
+
+#sudo rmmod messages_printer.ko
